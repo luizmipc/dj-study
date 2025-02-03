851c51e88278a4fd7e41c4bed5b3b8a2f4ad66f2
luizmipc <luizmipc@gmail.com>
2025-02-03 00:34:34 -0300

    ruff format

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from environs import Env
+
 env = Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
